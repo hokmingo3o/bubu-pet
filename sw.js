115f1978d22757d542f591995eb1cfa9d495888f
hokmingo3o <hokmingo3o@users.noreply.github.com>
2026-07-30 03:22:41 +0800

    卜卜桌面宠物

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'bubu-pet-v2';
+const FILES = [
+  './',
+  './index.html',
+  './bubu.png',
+  './bubu-icon.png',
+  './manifest.json',
+  './skin-panda.png',
+  './skin-miao-boy.png',
+  './skin-moutai.png',
+  './skin-miao-girl.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
